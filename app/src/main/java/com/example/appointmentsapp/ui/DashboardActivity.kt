9129f9d152ffa58287a748af426eb8abf4273f9b
JhonTincusi <ajhonjtc@gmail.com>
2024-01-01 16:43:37 -0500

    Adding new action in menu

--- app/src/main/java/com/example/appointmentsapp/ui/DashboardActivity.kt
+++ app/src/main/java/com/example/appointmentsapp/ui/DashboardActivity.kt
@@ -41,7 +41,7 @@ class DashboardActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_preferences
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/example/appointmentsapp/ui/preferences/PreferencesViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.appointmentsapp.ui.preferences
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PreferencesViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is preferences Fragment"
+    }
+    val text: LiveData<String> = _text
+}
