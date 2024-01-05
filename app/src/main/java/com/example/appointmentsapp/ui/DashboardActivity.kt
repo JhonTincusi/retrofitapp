6164f1b39eab4b287ba38317026348a76a850d2a
JhonTincusi <ajhonjtc@gmail.com>
2024-01-04 19:29:06 -0500

    fix menu

--- app/src/main/java/com/example/appointmentsapp/ui/DashboardActivity.kt
+++ app/src/main/java/com/example/appointmentsapp/ui/DashboardActivity.kt
@@ -1,27 +1,9 @@
 package com.example.appointmentsapp.ui
 
-import android.content.Context
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import android.view.KeyEvent
-import android.view.Menu
-import android.view.MenuItem
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import com.example.appointmentsapp.R
-import com.example.appointmentsapp.databinding.ActivityDashboardBinding
-import com.example.appointmentsapp.io.response.AuthorizationResponse
-import com.example.appointmentsapp.util.PreferenceHelper
-import com.example.appointmentsapp.util.PreferenceHelper.get
-import com.google.gson.Gson
+import android.content.Context import android.content.Intent import android.os.Bundle import android.util.Log import android.view.KeyEvent import android.view.Menu import android.view.MenuItem import android.widget.Toast import com.google.android.material.snackbar.Snackbar import com.google.android.material.navigation.NavigationView import androidx.navigation.findNavController import androidx.navigation.ui.AppBarConfiguration import androidx.navigation.ui.navigateUp import androidx.navigation.ui.setupActionBarWithNavController import androidx.navigation.ui.setupWithNavController import androidx.drawerlayout.widget.DrawerLayout import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import com.example.appointmentsapp.R import com.example.appointmentsapp.databinding.ActivityDashboardBinding import com.example.appointmentsapp.io.response.AuthorizationResponse import com.example.appointmentsapp.ui.gallery.GalleryFragment import com.example.appointmentsapp.ui.gallery.GalleryViewModel import com.example.appointmentsapp.ui.home.HomeViewModel import com.example.appointmentsapp.util.PreferenceHelper import com.example.appointmentsapp.util.PreferenceHelper.get import com.google.gson.Gson
 
 class DashboardActivity : AppCompatActivity() {
 
@@ -54,24 +36,75 @@ class DashboardActivity : AppCompatActivity() {
 
         if (authorizationResponseJson.isNotEmpty()) {
             val gson = Gson()
-            val authorizationResponse = gson.fromJson(authorizationResponseJson, AuthorizationResponse::class.java)
-
-            // Extraer la lista de nombres de módulos
-            val moduleNames = authorizationResponse.data.firstOrNull()?.systems?.firstOrNull()?.modules?.map { it.name } ?: listOf()
+            val authorizationResponse =
+                gson.fromJson(authorizationResponseJson, AuthorizationResponse::class.java)
+            val modules = authorizationResponse.data.firstOrNull()?.systems?.firstOrNull()?.modules ?: listOf()
             val menu = navView.menu
             menu.clear()
-            moduleNames.forEachIndexed { index, name ->
-                menu.add(Menu.NONE, Menu.FIRST + index, Menu.NONE, name).apply {
-                    icon = getDrawable(R.drawable.ic_menu_camera) // Replace with actual icon
+
+            modules.forEach { module ->
+                menu.add(Menu.NONE, module.module_id, Menu.NONE, module.name).apply {
+                    // Aquí puedes asignar íconos dinámicamente si tienes un mapeo de módulo a ícono
+                    icon = ContextCompat.getDrawable(this@DashboardActivity, R.drawable.ic_menu_camera) // Reemplaza con el ícono real
                 }
             }
-        }
-        else {
+
+            navView.setNavigationItemSelectedListener { menuItem ->
+                // Encuentra el módulo seleccionado basado en el ID del elemento del menú
+                val selectedModule = modules.find { it.module_id == menuItem.itemId }
+                when (selectedModule?.url) {
+                    "/category" -> {
+                        // Navegar a Categorías
+                        Log.d("xdd","catt")
+                    }
+                    "/subcategory" -> {
+                        // Navegar a Subcategorías
+                    }
+                    "/unit" -> {
+                        // Navegar a Unidades
+                    }
+                    "/presentation" -> {
+                        // Navegar a Presentaciones
+                    }
+                    "/product-input" -> {
+                        Log.d("xdd","catt")
+                        // Navegar a Ingreso de Productos
+                    }
+                    "/product-output" -> {
+                        // Navegar a Salida de Productos
+                    }
+                    "/product" -> {
+                        // Navegar a Productos
+                    }
+                    "/brand" -> {
+                        // Navegar a Marcas
+                    }
+                    "/product-subcategory" -> {
+                        // Navegar a Subcategoría de Productos
+                    }
+                    "/product-presentation" -> {
+                        // Navegar a Presentación de productos
+                    }
+                    "/specification" -> {
+                        // Navegar a Especificaciones
+                    }
+                    "/specification-detail" -> {
+                        // Navegar a Detalle de Especificación
+                    }
+                    "/product-detail" -> {
+                        // Navegar a Detalle de Productos
+                    }
+                    else -> {
+                        Toast.makeText(this, "Opción no reconocida", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+        } else {
             // Manejar el caso en que no hay datos guardados en SharedPreferences
         }
 
-
-        //val moduleNames = listOf("Module 1", "Module 2", "Module 3", "Module 4")
 
     }
 
@@ -92,6 +125,7 @@ class DashboardActivity : AppCompatActivity() {
                 logout(applicationContext)
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
